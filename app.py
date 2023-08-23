72a95a2709dbefd1090f5f189443f5533146ea02
94275793668 <gutaors@gmail.com>
2023-08-23 10:46:16 -0300

    adiciona minhas acoes

--- app.py
+++ app.py
@@ -21,11 +21,15 @@ n_dias = st.slider("Quantidade de dias de previsão", 30, 365)
 def pegar_dados_acoes():
     # path = "\\wsl.localhost\Ubuntu\home\gutao\dev\streamlit_docker\acoes\acoes.csv"
     path = "./acoes.csv"
-
     return pd.read_csv(path, delimiter=";")
 
+def pegar_minhas_acoes():
+    # path = "\\wsl.localhost\Ubuntu\home\gutao\dev\streamlit_docker\acoes\acoes.csv"
+    path = "./tickers.csv"
+    return pd.read_csv(path, delimiter=";")
 
 df = pegar_dados_acoes()
+df_minhas_acoes = pegar_minhas_acoes()
 
 acao = df["snome"]
 nome_acao_escolhida = st.sidebar.selectbox("Escolha uma ação:", acao)
@@ -92,3 +96,23 @@ df_treino = df_treino.rename(columns={"Date": "ds", "Close": "y"})
 # grafico2
 # grafico2 = plot_components_plotly(modelo, previsao)
 # st.plotly_chart(grafico2)
+
+df_minhas_acoes["ticker"] = df_minhas_acoes["ticker"].str.replace(".SA", "")
+
+ticker_selecionado = df_acao.iloc[0]["sigla_acao"]
+if ticker_selecionado in df_minhas_acoes["ticker"].values:
+    valor_pago = df_minhas_acoes[df_minhas_acoes["ticker"] == ticker_selecionado]["paguei"].values[0]
+    valor_maximo = df_valores["Close"].max()
+    diferenca_valor_maximo = valor_maximo - float(valor_pago.replace(",", "."))
+    valor_pago = float(valor_pago.replace(",", "."))
+    st.subheader("Análise da Ação: " + nome_acao_escolhida)
+    st.write("Ticker selecionado: " + ticker_selecionado)
+    st.write("Valor Pago: R$ {:.2f}".format(valor_pago))
+    st.write("Valor Máximo: R$ {:.2f}".format(valor_maximo))
+    st.write("Diferença entre Valor Máximo e Valor Pago: R$ {:.2f}".format(diferenca_valor_maximo))
+
+st.subheader("Valores em df_acao['sigla_acao']")
+st.write(df_acao["sigla_acao"])
+
+st.subheader("Valores em df_minhas_acoes['ticker']")
+st.write(df_minhas_acoes["ticker"])
